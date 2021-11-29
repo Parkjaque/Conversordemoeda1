fed88b9e3a52b4c9da3064c8d2f43a6d50f62897
Jaqueline Fogaça <83592978+Parkjaque@users.noreply.github.com>
2021-11-29 20:39:13 -0300

    Adicionar arquivos de projeto.

---
+++ Conversor/ConversorDeMoeda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ConversorDeMoeda
+{
+    private static float iof = 0.0295f;
+
+    static public void reaisParaDolar()
+    {
+        Console.Write("Qual é a cotação do dólar? ");
+        float cotaDolar = float.Parse(Console.ReadLine());
+
+        Console.Write("Quantos doláres você irá comprar? $");
+        float dolarParaComprar = float.Parse(Console.ReadLine());
+
+        float totalEmReais = dolarParaComprar * cotaDolar;
+
+        Console.WriteLine("Valor a ser pago em reais = R$ " + totalEmReais);
+        Console.WriteLine("Valor a ser pago de IOF = " + (ConversorDeMoeda.iof * 100) + "%");
+        Console.WriteLine("Valor total da compra com IOF = R$" + (totalEmReais + totalEmReais * ConversorDeMoeda.iof));
+    }
+}
